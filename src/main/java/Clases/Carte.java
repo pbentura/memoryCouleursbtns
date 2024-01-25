4e62171d68ac3e211ad5aa3115586b52e9d59592
pbentura <pinhasbent@gmail.com>
2024-01-25 11:08:05 +0100

    avec un seul évenement onBtnClicked

--- src/main/java/Clases/Carte.java
+++ src/main/java/Clases/Carte.java
@@ -3,7 +3,12 @@ package Clases;
 public class Carte {
     private String nom;
 
+    public String getNom() {
+        return nom;
+    }
+
     public Carte(String nom) {
         this.nom = nom;
     }
+
 }
--- src/main/java/com/example/tp7memory/MemoryController.java
+++ src/main/java/com/example/tp7memory/MemoryController.java
@@ -1,11 +1,20 @@
 package com.example.tp7memory;
 
+import Clases.Carte;
 import javafx.event.Event;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 
-public class MemoryController {
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.ResourceBundle;
 
+public class MemoryController {
+    int indiceBoutonClique;
+    Carte carteCliquee;
+
+    ArrayList<Carte> lesCartes;
     @javafx.fxml.FXML
     private Button btn1;
     @javafx.fxml.FXML
@@ -21,25 +30,51 @@ public class MemoryController {
     @javafx.fxml.FXML
     private Button btn16;
     @javafx.fxml.FXML
-    private Button btn17;
+    private Button btn10;
     @javafx.fxml.FXML
-    private Button btn18;
+    private Button btn9;
     @javafx.fxml.FXML
-    private Button btn19;
+    private Button btn8;
     @javafx.fxml.FXML
-    private Button btn110;
+    private Button btn4;
     @javafx.fxml.FXML
-    private Button btn111;
+    private Button btn7;
     @javafx.fxml.FXML
-    private Button btn112;
+    private Button btn3;
     @javafx.fxml.FXML
-    private Button btn113;
+    private Button btn6;
     @javafx.fxml.FXML
-    private Button btn114;
+    private Button btn5;
     @javafx.fxml.FXML
-    private Button btn115;
+    private Button btn2;
+
+    // TODO: Creer les cartes et les assigner aux labels
+    public void initialize(URL location, ResourceBundle resources) {
+        lesCartes = new ArrayList<Carte>();
+
+
+    }
+
+    public boolean cartRetournee() {
+        for (Carte carte : lesCartes) {
+            if (carteCliquee.) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     public void onBtnClicked(Event event) {
+        Button btnClique = (Button) event.getSource();
 
+        // Récupérer l'indice ou l'identifiant du bouton cliqué
+        indiceBoutonClique = Integer.parseInt(btnClique.getId().substring(3));
+
+        // Récupérer la carte associée
+        carteCliquee = lesCartes.get(indiceBoutonClique);
+
+        for (Button btn : new Button[]{btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn10, btn11, btn12, btn13, btn14, btn15, btn16}) {
+            if (btn.)
+        }
     }
 }
